0f465ffd68b866e690637c16d097fc929c6a4839
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-19 08:19:09 +0000

    a

---
+++ 01-basic/06-Num_and_math.js
@@ -0,0 +1,30 @@
+const score = 400;
+// console.log(score);
+const Balance = new Number(100)
+// console.log(Balance);
+
+// console.log(Balance.toString().length);
+// console.log(Balance.toFixed(2))
+const OthNumber = 123.9090;
+// console.log(OthNumber.toPrecision(4));
+const NumberZero = 1000000;
+// console.log(NumberZero.toLocaleString('en-IN'));
+
+
+//*************************maths  */
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,3,5,6,7));
+// console.log(Math.max(4,3,5,6,7));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min +1 ))+min)
