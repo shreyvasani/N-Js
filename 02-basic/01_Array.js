3fe3b28989b750caaa61fbd0f6e2b1db5cfe79cb
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-19 10:18:52 +0000

    new

---
+++ 02-basic/01_Array.js
@@ -0,0 +1,17 @@
+const myArr = [0,1,2,3,4,5];
+const myHeros = ["Shaktiman","naagraj","wohooo"]
+const myArr2 = new Array(1,2,3,4,5,6)
+
+// console.log(myArr[1]);
+
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop();
+
+// myArr.unshift(10)
+myArr.shift()
+
+console.log(myArr);
