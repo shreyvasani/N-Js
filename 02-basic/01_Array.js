10bcd80ea837d70c12920ae5e618a1f653f69256
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-19 10:42:17 +0000

    ..

--- 02-basic/01_Array.js
+++ 02-basic/01_Array.js
@@ -12,6 +12,25 @@ const myArr2 = new Array(1,2,3,4,5,6)
 // myArr.pop();
 
 // myArr.unshift(10)
-myArr.shift()
+// myArr.shift()
 
-console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(4));
+
+
+// const NewArr = myArr.join()
+
+// console.log(myArr);
+// console.log(NewArr);
+// console.log(typeof NewArr);
+
+
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+
+console.log("B",myArr);
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+log("C",myArr);
