b0b9ce113a0426755fa731e7383772fa59da7379
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-19 03:17:32 +0000

    good

--- 01-basic/03-ConversionOperation.js
+++ 01-basic/03-ConversionOperation.js
@@ -1,1 +1,29 @@
-let age ;
+let score = "23a";
+
+console.log(typeof(score));
+console.log(typeof score);
+
+
+let val = Number(score)
+console.log(typeof(val));
+console.log(val)
+
+
+//"33"=>33
+//"33abc"=> NAN
+//true => 1
+
+
+let isLoggedIn = 1
+let Bool = Boolean(isLoggedIn)
+console.log(Bool)
+
+//1=>true 0=>false
+//empty string => false
+//"shrey" => true
+
+
+let SomeNumber = 33
+let StringNumber = String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber)
