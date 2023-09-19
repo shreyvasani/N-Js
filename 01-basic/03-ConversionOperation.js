c9b547c291d35b68b775deff79d51fd3a0f54edd
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-19 03:48:37 +0000

    a

--- 01-basic/03-ConversionOperation.js
+++ 01-basic/03-ConversionOperation.js
@@ -1,12 +1,12 @@
 let score = "23a";
 
-console.log(typeof(score));
-console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof score);
 
 
 let val = Number(score)
-console.log(typeof(val));
-console.log(val)
+// console.log(typeof(val));
+// console.log(val)
 
 
 //"33"=>33
@@ -16,7 +16,7 @@ console.log(val)
 
 let isLoggedIn = 1
 let Bool = Boolean(isLoggedIn)
-console.log(Bool)
+// console.log(Bool)
 
 //1=>true 0=>false
 //empty string => false
@@ -25,5 +25,57 @@ console.log(Bool)
 
 let SomeNumber = 33
 let StringNumber = String(SomeNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber)
+
+// *******************Operations**********************
+
+let value = 3
+let negValue= -value
+// console.log(negValue)
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello"
+let str2 = " hi"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1+"1");
+// console.log("1"+1);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+
+
+let x = 3;
+const y = x++;
+console.log(x);
+console.log(y);
+
+
+
+let x1 = 3;
+const y1 = ++x1;
+console.log(x1);
+console.log(y1);
