a5274e5cf2e2ff860186e95e0d3b34afdab03ffa
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-19 09:36:51 +0000

    ..

---
+++ 01-basic/07-Date_Time.js
@@ -0,0 +1,27 @@
+let D = new Date();
+// console.log(D.toString());
+// console.log(D.toDateString());
+// console.log(D.toLocaleDateString());
+// console.log(D.toLocaleString());
+// console.log(typeof(D));
+
+
+// let myNewDate = new Date(2023,10,23)
+// let myNewDate = new Date("2023-01-14")
+// let myNewDate = new Date(2023,10,23,5,3)
+let myNewDate = new Date("01-14-2023")
+
+// console.log(myNewDate.toLocaleString());
+
+let timeStamps = Date.now()
+// console.log(timeStamps);
+// console.log(myNewDate.getTime());
+// console.log(Math.floor(Date.now())/1000);
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.getMonth());
+console.log(myDate.getDay());
+
+
+log
