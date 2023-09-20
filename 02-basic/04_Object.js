d596bd81a6fd59a5b3242bbac4359c93e1c67800
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-20 05:09:12 +0000

    ...

---
+++ 02-basic/04_Object.js
@@ -0,0 +1,58 @@
+const tinder = new Object() //using constructor singleton
+
+tinder.id = "123abc"
+tinder.name="Sam"
+tinder.isLoggedin=false
+
+
+// console.log(tinder);
+const regularUser = {
+    email:"some@gmail.com",
+    fullName:{
+        userfullname:{
+            fName: "shrey",
+            Lname:"vasani"
+        }
+    },
+}
+
+//console.log(regularUser.fullName.userfullname.fName);
+
+
+const obj1= {
+    1:"a",
+    2:"b"
+}
+
+const obj2= {
+    3:"c",
+    4:"d"
+}
+
+// const obj3 =Object.assign({},obj1,obj2);
+const obj3 = {...obj1,...obj2}
+//console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    }
+]
+
+users[1].email;
+
+console.log(tinder)
+console.log(Object.keys(tinder))
+console.log(Object.values(tinder))
+console.log(Object.entries(tinder))
+
+console.log(tinder.hasOwnProperty('isLoggedin'));
