dd5510b30f69e7643628b13174e023c453ef7927
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-20 11:20:05 +0000

    ..

---
+++ 04-ControlFlow/one.js
@@ -0,0 +1,47 @@
+//if statement
+// const isUserLoggedin = true;
+// const temperatuure = 41
+
+// if(temperatuure <= 50){
+//     console.log("YES < 50");
+// }else{
+// console.log("No>50");
+// }
+ // < , > , <= ,>= ,== , != , === ,!==
+
+
+//  const score = 200
+
+//  if(score > 100){
+//     const power = "fly"
+
+//     console.log(`user power:${power}`)
+//  }
+
+// const balance = 1000
+// if(balance > 500 ) console.log("test")
+// if(balance > 500 ) console.log("test"),console.log("test2");
+
+// if(balance < 500){
+//         console.log("Less than 500");
+// } else if(balance < 750){
+//     console.log("Less than 750");
+// }else if(balance<900){
+//     console.log("Less than 900");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+
+
+const userLoggedIn = true;
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+if (userLoggedIn && debitCard ){
+    console.log("Allow To buy course");
+} 
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("user logged in");
+}
