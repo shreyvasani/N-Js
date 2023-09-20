b9353d5c05763c7a2200b5ed342ec01e9845eee9
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-20 11:45:13 +0000

    ...

--- 04-ControlFlow/truthy.js
+++ 04-ControlFlow/truthy.js
@@ -1,11 +1,11 @@
-const userEmail = []
+// const userEmail = []
 
-if(userEmail){
-    console.log("got user Email");
-}
-else{
-    console.log("dont have user email");
-}   
+// if(userEmail){
+//     console.log("got user Email");
+// }
+// else{
+//     console.log("dont have user email");
+// }   
 
 // falsy value: false, 0 , -0 ,bigint 0n,"", null, undefined,NaN
 
@@ -14,9 +14,30 @@ else{
 // if(userEmail.length === 0){
 //     console.log("Array is Empty");
 // }
-const emptyObject = {}
+// const emptyObject = {}
 
-if(Object.keys(emptyObject).length === 0){
-    console.log("object is empty");
+// if(Object.keys(emptyObject).length === 0){
+//     console.log("object is empty");
 
-}
+// }
+
+
+
+// Nullish coalescing Operator (??):null undefined
+
+
+// let val1;
+//val1 = 5 ?? 10;
+//val1 = null ?? 10;
+//val1=undefined ?? 15
+
+
+// val1 = null ?? 10 ?? 15 ;
+// console.log(val1);
+
+
+// terniary opeartor
+// condition ? true :false
+
+const iceTeaprice = 100
+iceTeaprice <= 80 ? console.log("lessthan 80") : console.log("more than 80");
