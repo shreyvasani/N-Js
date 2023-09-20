0e9407be32a154b173d0fea703bb2f8c20a5e59f
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-20 06:39:03 +0000

    aa

--- 03-basic/02-scopes.js
+++ 03-basic/02-scopes.js
@@ -3,9 +3,45 @@ let a =300
 if(true){
     let a = 10;
     const b  = 20;
-    console.log("inner");
-    console.log(a);
+    // console.log("inner");
+    // console.log(a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
+
+
+function One() {
+    const username ="Shrey";
+
+    function Two() {
+        const website = "YT"
+        console.log(username);
+    }
+    // console.log(website);
+    Two()
+}
+//One();
+if(true){
+    const username = "SV"
+    if(username == "SV"){
+        const website = " TV"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// ################### INTERSTING ################
+console.log(addOne(3));
+function addOne (value) {
+    return value+1;
+}
+
+// addOne(3);
+// console.log(addTwo(3));//this wont be executed
+const addTwo= function(num) {
+    return num + 2
+}
+addTwo(2)
