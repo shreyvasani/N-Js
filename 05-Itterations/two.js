99623661ce2fb52e2226078482f24d6c509549bb
Shrey Vasani <47609218+shreyvasani@users.noreply.github.com>
2023-09-21 06:28:28 +0000

    ...

---
+++ 05-Itterations/two.js
@@ -0,0 +1,23 @@
+//while loop
+// let i = 0;
+
+// while (i<=10) {
+//     console.log(`value of i is ${i}`);
+//     i = i + 2;
+// }
+
+// let myArr = ["flash","batman","superman"]
+// let arr = 0
+// while(arr < myArr.length){
+//     console.log(`value is ${myArr[arr]}`);
+//     arr = arr + 1;
+// }
+
+
+//do-while loop
+
+let Score =11;
+do {
+    console.log(`score is ${Score}`);
+    Score++;
+} while (Score<= 10);
